82cf1dd9f00eb7edf379f1bbdeeb46f99f70f672
E.Z. Hart <hartez@gmail.com>
2022-06-23 15:26:54 -0600

    Convert some tests to see what we're getting into

--- src/Controls/tests/Core.UnitTests/BaseTestFixture.cs
+++ src/Controls/tests/Core.UnitTests/BaseTestFixture.cs
@@ -40,4 +40,33 @@ namespace Microsoft.Maui.Controls.Core.UnitTests
 			DispatcherProvider.SetCurrent(null);
 		}
 	}
+
+	public class BaseTestFixtureXUnit : IDisposable
+	{
+		CultureInfo _defaultCulture;
+		CultureInfo _defaultUICulture;
+
+		public BaseTestFixtureXUnit() 
+		{
+			Microsoft.Maui.Controls.Hosting.CompatibilityCheck.UseCompatibility();
+			_defaultCulture = System.Threading.Thread.CurrentThread.CurrentCulture;
+			_defaultUICulture = System.Threading.Thread.CurrentThread.CurrentUICulture;
+			MockPlatformSizeService.Current?.Reset();
+			DispatcherProvider.SetCurrent(new DispatcherProviderStub());
+			DeviceDisplay.SetCurrent(null);
+			DeviceInfo.SetCurrent(null);
+			AppInfo.SetCurrent(null);
+		}
+
+		public void Dispose()
+		{
+			MockPlatformSizeService.Current?.Reset();
+			AppInfo.SetCurrent(null);
+			DeviceDisplay.SetCurrent(null);
+			DeviceInfo.SetCurrent(null);
+			System.Threading.Thread.CurrentThread.CurrentCulture = _defaultCulture;
+			System.Threading.Thread.CurrentThread.CurrentUICulture = _defaultUICulture;
+			DispatcherProvider.SetCurrent(null);
+		}
+	}
 }
